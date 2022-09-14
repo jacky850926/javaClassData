076e091895c8129d719dc047c086ee4b3cd74622
weber <weber122462118@gmail.com>
2022-09-15 02:13:28 +0800

    0914 半夜做SQL語句

--- 專題飯店/SQLQuery2.sql
+++ 專題飯店/SQLQuery2.sql
@@ -4,4 +4,22 @@ select * from room;
 
 select * from reservation;
 
+select * from roomstyle;
+
 select sum(numberOfRoom) from reservation group by roomID
+
+select *,'2022-09-11' checkInDate,'2022-09-12' checkOutDate from room cross join roomstyle;
+
+select roomID,'2022-09-11' checkInDate,'2022-09-12' checkOutDate from room;
+
+select reservationID,roomID,checkInDate,checkOutDate from reservation where checkInDate in ('2022-09-11','2022-09-12') or checkoutdate in ('2022-09-11','2022-09-12');
+
+select roomID,'2022-09-11' checkInDate,'2022-09-12' checkOutDate from room where roomID not in ((select roomID from reservation where checkInDate in ('2022-09-11','2022-09-12') or checkoutdate in ('2022-09-11','2022-09-12')));
+
+select * from room where roomID not in  ((select roomID from reservation where checkInDate in ('2022-09-11','2022-09-12') or checkoutdate in ('2022-09-11','2022-09-12')))
+
+select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-09-12' and '2022-09-14' or checkoutdate between '2022-09-12' and '2022-09-14')
+
+select roomID from reservation where checkInDate between '2022-09-12' and '2022-09-14' or checkoutdate between '2022-09-12' and '2022-09-14'
+
+select * from room e1 join reservation e2 on e1.roomID  = e2.roomID
