657fd30cae1e52653f000c13f6f30b7cbb8eea4c
weber <weber122462118@gmail.com>
2022-10-01 03:26:41 +0800

    221001 SQL歐齁齁齁

--- 專題飯店/SQLQuery2.sql
+++ 專題飯店/SQLQuery2.sql
@@ -22,7 +22,7 @@ select roomID,'2022-09-11' checkInDate,'2022-09-12' checkOutDate from room where
 
 select * from room where roomID not in  ((select roomID from reservation where checkInDate in ('2022-09-11','2022-09-12') or checkoutdate in ('2022-09-11','2022-09-12')))
 --目的應該是抓出有房間的飯店名字
-select * from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-09-12' and '2022-09-14' or checkoutdate between '2022-09-12' and '2022-09-14')) as a join roomstyle as b on a.roomStyleID = b.styleID join hotel as c on b.hotelID = c.hotelID where c.name = 'qweq' and b.capacity > 2 order by b.hotelID
+select * from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-09-11' and '2022-09-12' or checkoutdate between '2022-09-11' and '2022-09-12')) as a join roomstyle as b on a.roomStyleID = b.styleID join hotel as c on b.hotelID = c.hotelID where c.country = '台灣' and b.capacity > 2 order by b.hotelID
 
 --不join的找法
 select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-09-12' and '2022-09-14' or checkoutdate between '2022-09-12' and '2022-09-14') order by roomStyleID
@@ -33,4 +33,5 @@ select * from room e1 join reservation e2 on e1.roomID  = e2.roomID
 
 
 select c.hotelID,c.name,c.address,c.averagePrice,count(*) amount from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-10-11' and '2022-10-12' or checkoutdate between '2022-10-11' and '2022-10-12')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.country = '台灣' and b.capacity > 2 group by c.hotelID,c.name,c.address,c.averagePrice
-
+---飯店頁面內找房間
+select b.* from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-9-11' and '2022-9-12' or checkoutdate between '2022-9-11' and '2022-9-12')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.hotelID = 6 and b.capacity > 2 
