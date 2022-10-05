66885076d7d7fe3f1a821ad356ac1ca6d7e0b4fe
weber <weber122462118@gmail.com>
2022-10-05 15:23:10 +0800

    Merge branch 'master' of https://github.com/siaball/javaClassData
    # Conflicts:
    #	專題飯店/221002jacky.sql

--- 專題飯店/SQLQuery2.sql
+++ 專題飯店/SQLQuery2.sql
@@ -30,9 +30,13 @@ select * from room where roomID not in  (select roomID from reservation where ch
 select roomID from reservation where checkInDate between '2022-09-12' and '2022-09-14' or checkoutdate between '2022-09-12' and '2022-09-14'
 
 select * from room e1 join reservation e2 on e1.roomID  = e2.roomID
+select * from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-10-04' and '2022-10-04' or checkoutdate between '2022-10-04' and '2022-10-04')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.country = '台灣' and b.capacity > 2
 
 
-select c.hotelID,c.name,c.address,c.averagePrice,count(*) amount from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-10-11' and '2022-10-12' or checkoutdate between '2022-10-11' and '2022-10-12')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.country = '台灣' and b.capacity > 2 group by c.hotelID,c.name,c.address,c.averagePrice
+select c.hotelID,c.name,c.address,c.averagePrice,count(*) amount from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-10-04' and '2022-10-04' or checkoutdate between '2022-10-04' and '2022-10-04')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.country = '台灣' and b.capacity > 2 group by c.hotelID,c.name,c.address,c.averagePrice
 ---飯店頁面內找房間
-select b.styleID,b.name,b.price,b.capacity,b.bed,count(*) roomAmount from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-9-18' and '2022-9-19' or checkoutdate between '2022-9-18' and '2022-9-19')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.hotelID = 7 and b.capacity > 2 group by b.styleID,b.name,b.price,b.capacity,b.bed
+select b.styleID,b.name,b.price,b.capacity,b.bed,count(*) roomAmount from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-9-18' and '2022-9-19' or checkoutdate between '2022-9-18' and '2022-9-19')) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where c.hotelID = 8 and b.capacity > 2 group by b.styleID,b.name,b.price,b.capacity,b.bed
 
+select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-10-04' and '2022-10-04' or checkoutdate between '2022-10-04' and '2022-10-04') and roomStyleID = 8
+
+select * from (select * from room where roomID not in  (select roomID from reservation where checkInDate between '2022-10-04' and '2022-10-04' or checkoutdate between '2022-10-04' and '2022-10-04') ) as a JOIN roomstyle as b on a.roomStyleID = b.styleID JOIN hotel as c on b.hotelID = c.hotelID where roomStyleID = 8 and b.capacity > 2
